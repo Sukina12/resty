c214231b630e01cf62d28b42dd3b9666632ea71f
sukina12 <sukina_89@yahoo.com>
2021-08-13 09:37:20 +0300

    done lab28

--- src/components/results/Results.jsx
+++ src/components/results/Results.jsx
@@ -1,29 +1,59 @@
-import React from 'react';
-import './results.scss';
-import JSONPretty from 'react-json-pretty';
-import 'react-json-pretty/themes/monikai.css';
-
-function Results (props){
-  console.log('resultData',props.data);
-
-  return (
-    <div data-testid="result"  >
-      { props.data &&
-      <>
-       "count : "
-       <JSONPretty data ={props.data.count} />
-       "headers : "
-       <JSONPretty data ={props.data.headers}/> 
-       "results : "
-       <JSONPretty data ={props.data.results} />
-       </>
-      }
-      
-     
-
-    </div>
+import React from "react";
+import "./results.scss";
+import JSONPretty from "react-json-pretty";
+import "react-json-pretty/themes/adventure_time.css";
+import { useState } from "react";
+function Results(props) {
+  console.log(props);
+  const fake = {
+    "cache-control": "public, max-age=86400, s-maxage=86400",
+    "content-type": "application/json; charset=utf-8",
+  };
+  const [methodsData, setMethodsData] = useState(
+    JSON.parse(localStorage.getItem("memory")) || []
   );
 
+  return (
+    <>
+      <section className="List">
+        <h2>MethodsMemory :</h2>
+        {methodsData.map((item, idx) => (
+          <div>
+            <button
+              className={item.method}
+              key={idx}
+              // onClick={fetch}
+              // ref={node => (inputNode = node)}
+            >
+              {item.method}
+            </button>
+            <p>{item.url}</p>
+          </div>
+        ))}
+      </section>
+      <section>
+        <h3>Count</h3>
+        <JSONPretty
+          data={
+            props.data.data.results
+              ? props.data.data.results.length
+              : props.data.data.data.length
+          }
+        ></JSONPretty>
+        <h3>Headers</h3>
+        <JSONPretty
+          data={props.data.headers ? props.data.headers : fake}
+        ></JSONPretty>
+        <h3>Results</h3>
+        <JSONPretty
+          data={
+            props.data.data.results
+              ? props.data.data.results
+              : props.data.data.data
+          }
+        ></JSONPretty>
+      </section>
+    </>
+  );
 }
-
 export default Results;
